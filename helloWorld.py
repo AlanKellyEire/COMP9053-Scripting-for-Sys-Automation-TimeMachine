5ef613e461a35fc76eaffd85a5d68093e13009b2
Alankellyeire <alan.kelly1@mycit.ie>
2018-10-26 23:23:14 +0100

    added print statements

--- helloWorld.py
+++ helloWorld.py
@@ -1,17 +1,20 @@
 import sys
+import os
 import time
 import logging
 from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler
 
 if __name__ == "__main__":
+    cwd = os.getcwd()
+    print (cwd)
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
     path = sys.argv[1] if len(sys.argv) > 1 else '.'
     event_handler = LoggingEventHandler()
     observer = Observer()
-    observer.schedule(event_handler, path, recursive=True)
+    observer.schedule(event_handler, path, recursive=False)
     observer.start()
     try:
         while True:
